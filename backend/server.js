e12ec9fd6c09f29d83498fa2489a77bb957db389
tyang0119 <tianhao@wustl.edu>
2020-12-09 20:31:26 -0800

    init

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,17 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 4000
+console.log("Starting server")
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
 
 //set mongoose
-mongoose.connect(PORT, () => console.log(`Server started on PORT : ${PORT}`))
+console.log("Connecting to MongoDB")
+mongoose.connect("mongodb://127.0.0.1:27017/react-blog-project", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) return console.error(err);
+    console.log("MongoDB connection established")
+})
